b6826bde17e07660c41f707bebd6b324510cac41
museumgijutsu <gijutsu@museum.hokudai.ac.jp>
2026-07-15 14:03:08 +0900

    Update index.js

--- index.js
+++ index.js
@@ -22,6 +22,15 @@ async function init() {
     });
     mapElement.append(marker2);
     mapElement.append(marker);
+
+    const posTbl = [position:{43.065866, 141.362682},
+                    position:{43.062961, 141.353696},
+                    position:{43.064740, 141.346918},
+                    ];
+    for (let i = 0; posTbl.length > i; ++i) {
+        mapElement.append(new AdvancedMarkerElement({position[i]});
+    }
+        
 }
 
 void init();
