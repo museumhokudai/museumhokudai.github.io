6ae4517a3128212199d42e61ef905939eebc51cd
museumgijutsu <gijutsu@museum.hokudai.ac.jp>
2026-07-16 11:37:23 +0900

    Update index.js

--- index.js
+++ index.js
@@ -23,13 +23,13 @@ async function init() {
     mapElement.append(marker2);
     mapElement.append(marker);
 
-    const posTbl = [position:{43.065866, 141.362682},
-                    position:{43.062961, 141.353696},
-                    position:{43.064740, 141.346918},
+    const posTbl = [{position:{lat:43.065866, lng:141.362682},},
+                    {position:{lat:43.062961, lng:141.353696},},
+                    {position:{lat:43.064740, lng:141.346918},},
                     ];
     for (let i = 0; posTbl.length > i; ++i) {
-        console.log(position[i]);
-        mapElement.append(new AdvancedMarkerElement({position[i]});
+        console.log(posTbl[i]);
+        mapElement.append(new AdvancedMarkerElement(posTbl[i]);
     }
         
 }
