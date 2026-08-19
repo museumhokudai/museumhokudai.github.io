c272a1e8c778bb83ec4dba557c9af500f6fe7672
museumgijutsu <gijutsu@museum.hokudai.ac.jp>
2026-08-19 13:50:03 +0900

    Create showmap.js

---
+++ showmap.js
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>Default Advanced Marker5</title>
+
+        <link rel="stylesheet" type="text/css" href="./style.css" />
+        <script type="module" src="./index.js"></script>
+        <script>
+            // prettier-ignore
+            (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
+                key: "AIzaSyALja_kFsmAlSiCW1pBQBfU5gRs4PRySuE"
+            });
+        </script>
+    </head>
+    <body>
+        <gmp-map
+            center="43.07279381676491,141.3422381221747"
+            zoom="14"
+            map-id="4504f8b37365c3d0"></gmp-map>
+    </body>
+</html>
